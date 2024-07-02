94dfc45a99960f22de92df9b1ef0c0d2759006d1
aitnacer-nabil <palancia.gor@gmail.com>
2024-07-02 21:55:42 +0100

    init Project and set up html template and create a navbar

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from "./component/Navbar/Navbar";
+import {DarkTheme} from "./Theme/DarkTheme";
+import {CssBaseline, ThemeProvider} from "@mui/material";
+
+
+function App() {
+    return (
+
+
+    <div>
+        <Navbar/>
+    </div>
+
+    );
+}
+
+export default App;
